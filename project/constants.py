3af4346f94bc8efb0a5308eeb0bd21dfbe28a834
kim-taewoo <acoustic0419@gmail.com>
2019-10-27 22:02:17 +0900

    first commit

---
+++ project/constants.py
@@ -0,0 +1,7 @@
+CONSUMER_KEY = 'pgsioHGD50KXBOaPpQcm3ZWUu'
+CONSUMER_SECRET = 'Rtoo0lk7SHESxAsVrwMkEj4Apjw59q5ydsqxA5O3SAGcX28EfK'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+
